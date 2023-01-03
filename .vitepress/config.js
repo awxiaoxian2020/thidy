c5e4984abc5e567dc4c1ef27790cf3f4dfa0af91
Xavi Lee <awxiaoxian2020@163.com>
2023-01-03 21:34:43 +0800

    feat: add contents and config themes

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,5 +4,13 @@ export default {
     lastUpdated: true,
     titleTemplate: false,
     srcDir: './src',
-    outDir: './dist'
+    outDir: './dist',
+    themeConfig: {
+        outlineTitle: '导航',
+        lastUpdatedText: '最后更新于',
+        editLink: {
+            pattern: 'https://github.com/awxiaoxian2020/thidy/edit/master/src/:path',
+            text: '在 GitHub 编辑此页面'
+        }
+      }
 }
