46e2e3070b75148bd7f6d75e37d9368462fd5cb7
Xavi Lee <awxiaoxian2020@163.com>
2023-01-29 20:09:04 +0800

    feat: update and add ICP

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,11 +6,16 @@ export default {
     srcDir: './src',
     outDir: './dist',
     themeConfig: {
-        outlineTitle: '导航',
+        outline: {
+            label: '页面导航'
+        },
         lastUpdatedText: '最后更新于',
         editLink: {
             pattern: 'https://github.com/awxiaoxian2020/thidy/edit/master/src/:path',
             text: '在 GitHub 编辑此页面'
+        },
+        footer: {
+            message: '<a href="https://beian.miit.gov.cn">皖ICP备2021004162号-2</a>'
         }
       }
 }
